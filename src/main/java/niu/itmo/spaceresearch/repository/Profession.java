1a5e253633a5e91f1439bf47864693a93cfe6e35
AmiFideles <251645@niuitmo.ru>
2023-12-13 16:43:27 +0300

    commit

--- src/main/java/niu/itmo/spaceresearch/repository/Profession.java
+++ src/main/java/niu/itmo/spaceresearch/repository/Profession.java
@@ -1,10 +1,9 @@
 package niu.itmo.spaceresearch.repository;
 
-import niu.itmo.spaceresearch.model.Profession;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 /**
  * @author amifideles
  */
-public interface RoleRepository extends JpaRepository<Profession, Long> {
+public interface Profession extends JpaRepository<Profession, Long> {
 }
--- src/main/java/niu/itmo/spaceresearch/repository/Researcher.java
+++ src/main/java/niu/itmo/spaceresearch/repository/Researcher.java
@@ -1,11 +1,10 @@
 package niu.itmo.spaceresearch.repository;
 
-import niu.itmo.spaceresearch.model.Researcher;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 /**
  * @author amifideles
  */
 
-public interface UserRepository extends JpaRepository<Researcher, Long> {
+public interface Researcher extends JpaRepository<Researcher, Long> {
 }
